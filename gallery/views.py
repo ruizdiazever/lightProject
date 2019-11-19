894f23b027a578c3bfde5f3db50da67d62d4afd8
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-11-18 22:40:20 -0300

    New app 'gallery'

---
+++ gallery/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def bariloche(request):
+    return render(request, "gallery/bariloche.html")
--- ligth/urls.py
+++ ligth/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core import views
+from core import views as views_core
+from gallery import views as views_gallery
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
-    path('equipment/', views.equipment, name='equipment'),
+    path('home/', views_core.home, name='home'),
+    path('equipment/', views_core.equipment, name='equipment'),
+    path('bariloche/', views_gallery.bariloche, name='bariloche'),
 ]
