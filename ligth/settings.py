dd3e7c3842233570019fb54add9cf387f584c8ce
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-12-03 23:42:02 -0300

    Changes

--- ligth/settings.py
+++ ligth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '94^u#7dw%u@!_&a#)lip+))zat755f&#3sc3-70catb)v@x#4j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['everdev.pythonanywhere.com','localhost','127.0.0.1']
 
