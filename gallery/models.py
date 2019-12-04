693de1ecc076abfbf09c9d177ab341aaa59cf162
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-12-03 23:49:02 -0300

    Changes

--- gallery/models.py
+++ gallery/models.py
@@ -5,7 +5,6 @@ class Photo(models.Model):
     title = models.CharField(max_length=200, verbose_name = "Titulo") 
     description = models.TextField(verbose_name = "Descripcion")
     image = models.ImageField(verbose_name = "Imagen", upload_to="photos")
-    link = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name = "Fecha de modificacion")
 
